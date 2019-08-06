2a5cc2756629f1686e9161e68e6a921b186bf682
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-07 00:16:20 +0100

    made a ProjectsObj class (object) so that I can just reference each property using Blazor C# in the HTML

---
+++ blazorWOO/Pages/ProjectsObj.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorWOO.Pages
+{
+    public class ProjectsObj
+    {
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string subtitle;
+
+        public string Subtitle
+        {
+            get { return subtitle; }
+            set { subtitle = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+
+    }
+}
