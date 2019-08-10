6c73a76eb57725f9f36f368a6271499c956120ad
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-10 14:37:57 +0100

    made a class for getting and setteing input

---
+++ blazorWOO/Pages/input.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorWOO.Pages
+{
+    public class input
+    {
+        public input()
+        {
+
+        }
+
+        public string type { get; set; }
+        public string placeholder { get; set; }
+    }
+}
