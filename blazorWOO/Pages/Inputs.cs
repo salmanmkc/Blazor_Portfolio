0b54909e2a967f69bc5a1835b16e062b86542f53
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-10 14:40:50 +0100

    created a class Inputs to instantiate input classes

---
+++ blazorWOO/Pages/Inputs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorWOO.Pages
+{
+    public class Inputs
+    {
+        public Inputs()
+        {
+            input i1 = new input("text", "First Name");
+            input i2 = new input("text", "Second Name");
+            input i3 = new input("email", "Email Address");
+            input i4 = new input("date", "Date of Birth");
+
+        }
+    }
+}
