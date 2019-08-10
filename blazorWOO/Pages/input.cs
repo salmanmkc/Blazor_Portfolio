f0c2c7b765a43506c4b37e85360c475c51a265e5
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-10 14:38:57 +0100

    set up the constructor

--- blazorWOO/Pages/input.cs
+++ blazorWOO/Pages/input.cs
@@ -7,9 +7,10 @@ namespace blazorWOO.Pages
 {
     public class input
     {
-        public input()
+        public input(string Type, string Placeholder)
         {
-
+            type = Type;
+            placeholder = Placeholder;
         }
 
         public string type { get; set; }
