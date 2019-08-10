d68f7c753bdae56a1acb38aad7a870dd96faf948
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-10 22:28:18 +0100

    added overload for 3 images and 5 paragraphs

--- blazorWOO/Pages/post.cs
+++ blazorWOO/Pages/post.cs
@@ -49,6 +49,18 @@ namespace blazorWOO.Pages
             Para4 = para4;
         }
 
+        public post(string para1, string img1, string para2, string img2, string para3, string im3, string para4, string para5)
+        {
+            Para1 = para1;
+            Img1 = img1;
+            Para2 = para2;
+            Img2 = img2;
+            Para3 = para3;
+            Img3 = img3;
+            Para4 = para4;
+            Para5 = para5;
+        }
+
         private string para1;
 
         public string Para1
