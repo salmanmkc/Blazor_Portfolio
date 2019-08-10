936a685521c50cdb4c8ce5cd80d39ae6c13f0996
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-10 22:22:28 +0100

    made get and set methods for paragraphs and images

---
+++ blazorWOO/Pages/post.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorWOO.Pages
+{
+    public class post
+    {
+        public post()
+        {
+
+        }
+
+        private string para1;
+
+        public string Para1
+        {
+            get { return para1; }
+            set { para1 = value; }
+        }
+
+        private string para2;
+
+        public string Para2
+        {
+            get { return para2; }
+            set { para2 = value; }
+        }
+        private string para3;
+
+        public string Para3
+        {
+            get { return para3; }
+            set { para3 = value; }
+        }
+
+        private string img1;
+
+        public string Img1
+        {
+            get { return img1; }
+            set { img1 = value; }
+        }
+
+        private string img2;
+
+        public string Img2
+        {
+            get { return img2; }
+            set { img2 = value; }
+        }
+        private string img3;
+
+        public string Img3
+        {
+            get { return img3; }
+            set { img3 = value; }
+        }
+
+    }
+}
